d2b36baffd5a3ab77772eed17d9ddf06412b18e9
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 21:42:16 +0700

    Create petanikode5_1.c

---
+++ petanikode5_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // membuat variabel dengan tipe data integer
+    int berat;
+    // membuat variabel dan langsung mengisinya
+    int tinggi = 178;
+
+    // mengisi nilai ke variabel
+    berat = 54;
+
+    // mencetak isi variabel ke layar
+    printf("Berat badan saya adalah %ikg dan tinggi %icm", berat, tinggi);
+
+    return 0;
+}
