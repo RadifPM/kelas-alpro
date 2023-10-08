de91917e60dc9f56e9b7f4acaba995c6a09355f3
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 21:43:56 +0700

    Create petanikode6_1.c

---
+++ petanikode6_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+
+    int a, b, c;
+
+    printf("Inputkan nilai a: ");
+    scanf("%i", &a);
+
+    printf("Inputkan nilai b: ");
+    scanf("%i", &b);
+
+    c = a + b;
+
+    printf("Hasil a + b: %i", c);
+
+}
