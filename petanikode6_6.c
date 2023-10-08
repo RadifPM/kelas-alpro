96752a1a90f21c04ad42cc29863355c50faf3c56
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 21:45:25 +0700

    Create petanikode6_6.c

---
+++ petanikode6_6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+    int a = 1; // true
+    int b = 0; // false
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    // logika AND
+    printf("a && b = %d\n", a && b);
+
+    // logika OR
+    printf("a || b = %d\n", a || b);
+
+    // logika NOT
+    printf("!a = %d\n", !a);
+}
