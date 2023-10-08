a759e767ee5e791d4e74dec4467fc65625a3c6b0
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 21:41:50 +0700

    Create petanikode4_7.c

---
+++ petanikode4_7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    char name[50], email[50];
+    
+    printf("Nama: ");
+    fgets(name, sizeof(name), stdin);
+    
+    printf("Email: ");
+    fgets(email, sizeof(email), stdin);
+   
+    printf("\n-------------------------\n");
+    printf("Nama anda: %s", name);
+    printf("Alamat email: %s", email);
+    
+    return 0;
+}
