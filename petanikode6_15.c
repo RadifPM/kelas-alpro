90222b1d89a94a0e87077c79def263e333ec770c
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 22:19:33 +0700

    Create petanikode6_15.c

---
+++ petanikode6_15.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main(){
+    int a = 4;
+
+    // increment a
+    a++;
+
+    printf("a++ = %d\n", a);
+
+    // increment a
+    ++a;
+
+    printf("++a = %d\n", a);
+
+    // decrement a
+    a--;
+
+    printf("a-- = %d\n", a);
+
+    // decrement a
+    --a;
+
+    printf("--a = %d\n", a);
+}
