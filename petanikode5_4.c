f2e8a9a193a7a612e6735b2245b2dc249aa62ca7
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 21:43:28 +0700

    Create petanikode5_4.c

---
+++ petanikode5_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    const double PI = 3.14;
+    const char JENIS_KELAMIN = 'P';
+    const int VERSI = 11;
+
+    printf("isi konstanta PI adalah %f\n", PI);
+    printf("isi konstanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);
+    printf("isi konstanta VERSI adalah %i\n", VERSI);
+}
