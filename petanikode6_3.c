65c337cbc5b21c4d67e32105153bb61a53f00aa7
RadifPM <112796714+RadifPM@users.noreply.github.com>
2023-10-08 21:44:37 +0700

    Create petanikode6_3.c

---
+++ petanikode6_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+
+    float a, b;
+
+    printf("Inputkan nilai a: ");
+    scanf("%i", &a);
+
+    printf("Inputkan nilai b: ");
+    scanf("%i", &b);
+
+    printf("Hasil a / b: %f\n", a / b);
+
+}
